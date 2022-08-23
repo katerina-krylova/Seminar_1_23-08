2604de3513069f632892a074c1ce1b1db2f510db
Katerina Krylova <katerina-zaytseva@yandex.com>
2022-08-23 14:53:52 +0300

    Создано решение к задаче 3

---
+++ Task_3/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Write("Введите число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+if (numberA % 2 == 0) Console.WriteLine("Четное");
+else Console.WriteLine("Нечетное");
+Console.ReadKey();
