385fe21651aa579919087484a83c4e19156e9dff
Katerina Krylova <katerina-zaytseva@yandex.com>
2022-08-23 10:17:02 +0300

    Создано решение к задаче 1

---
+++ Task_1/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите первое число: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+int max = firstNumber;
+int min = secondNumber;
+if (firstNumber > max) max= firstNumber;
+if (secondNumber > max) max= secondNumber;
+if (secondNumber < min) min= secondNumber;
+if (firstNumber < min) min= firstNumber;
+Console.Write ("max =");
+Console.WriteLine (max);
+Console.Write ("min =");
+Console.WriteLine (min);
+
+
