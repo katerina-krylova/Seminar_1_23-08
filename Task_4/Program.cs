68314c0d79639aae74a1fd1741734e778608198a
Katerina Krylova <katerina-zaytseva@yandex.com>
2022-08-23 16:04:05 +0300

    Создано решение к задаче 4. Задачу до конца не смогла решить

---
+++ Task_4/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int startNumber = 1;
+while(startNumber <= number)
+{
+Console.Write(startNumber + " ");
+startNumber++;
+}
